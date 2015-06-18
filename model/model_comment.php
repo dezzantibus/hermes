e39156e2dcea011ced28014810d170b9ea80c12a
Andrea Passante <andrea.passante@firmstep.com>
2015-06-18 09:14:25 +0100

    Added model shells

---
+++ model/model_comment.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zante
+ * Date: 17/06/2015
+ * Time: 17:02
+ */
+
+class model_comment extends model
+{
+
+    static public function create()
+    {
+
+    }
+
+    static public function update()
+    {
+
+    }
+
+    static public function delete()
+    {
+
+    }
+
+    static public function getById( $id )
+    {
+
+    }
+
+}
---
+++ model/model_journalist.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zante
+ * Date: 17/06/2015
+ * Time: 17:02
+ */
+
+class model_journalist extends model
+{
+
+    static public function create()
+    {
+
+    }
+
+    static public function update()
+    {
+
+    }
+
+    static public function delete()
+    {
+
+    }
+
+    static public function getById( $id )
+    {
+
+    }
+
+    static public function getByUrl( $url )
+    {
+
+    }
+
+}
