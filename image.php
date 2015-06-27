5c799010821f609eff53eac404edf862feb22d14
Andrea Passante <zante@inwind.it>
2015-06-27 16:32:41 +0100

    Automated filler images

---
+++ image.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zante
+ * Date: 17/06/2015
+ * Time: 11:49
+ */
+
+class image
+{
+
+    const PROCESSED_PATH = '../images/uploads/processed/';
+
+    const ORIGINAL_PATH = '../images/uploads/original/';
+
+    public static function retrieve( $file, $width, $height )
+    {
+
+        $processed_filename = self::PROCESSED_PATH . $file . '_' . $width . '_' . $height . '.jpg';
+
+        if( file_exists( $processed_filename ) )
+        {
+            $image = imagecreatefromjpeg( $processed_filename );
+        }
+        else
+        {
+
+            $original_filename = self::ORIGINAL_PATH . $file . '.jpg';
+
+            $image = imagecreatefromjpeg( $original_filename );
+
+            $original_width  = imagesx( $image );
+            $original_height = imagesy( $image );
+
+            if( ( $original_width / $width ) < ( $original_height / $height ) )
+            {
+                $new_height = $height;
+                $new_width  = $original_width * ( $height / $original_height );
+            }
+            else
+            {
+                $new_width  = $width;
+                $new_height = $original_height * ( $width / $original_width );
+            }
+
+            $image = imagescale( $image, $new_width, $new_height );
+
+            $cropping_array = array(
+                'x'      => ( $new_width - $width ) / 2,
+                'y'      => ( $new_height - $height ) / 2,
+                'width'  => $width,
+                'height' => $height,
+            );
+
+            $image = imagecrop( $image, $cropping_array );
+
+            imagejpeg( $image, $processed_filename );
+
+        }
+
+        header('Content-Type: image/jpeg');
+        imagejpeg( $image );
+
+    }
+
+}
+
+if( $_GET['file'] == 'filler.jpg' )
+{
+
+    header('Content-Type: image/jpeg');
+
+    $test = rand(1, 13);
+    error_log($test);
+    switch( $test )
+    {
+        case 1:  $category = '/cats/'; break;
+        case 2:  $category = '/abstract/'; break;
+        case 3:  $category = '/animals/'; break;
+        case 4:  $category = '/business/'; break;
+        case 5:  $category = '/city/'; break;
+        case 6:  $category = '/food/'; break;
+        case 7:  $category = '/nightlife/'; break;
+        case 8:  $category = '/fashion/'; break;
+        case 9:  $category = '/people/'; break;
+        case 10: $category = '/nature/'; break;
+        case 11: $category = '/sports/'; break;
+        case 12: $category = '/technics/'; break;
+        case 13: $category = '/transport/'; break;
+    }
+
+    imagejpeg( imagecreatefromjpeg( 'http://lorempixel.com/' . $_GET['width'] . '/' . $_GET['height'] . $category  ) );
+    die();
+}
+
+image::retrieve( $_GET['file'], $_GET['width'], $_GET['height'] );
--- layout/homepage/layout_homepage_hero.php
+++ layout/homepage/layout_homepage_hero.php
@@ -27,7 +27,7 @@ class layout_homepage_hero extends layout
                 <div class="flexslider">
                     <ul class="slides">
                         <li>
-                            <img src="demo/785x505.gif" alt="Image" />
+                            <img src="/785/505/filler.jpg" alt="Image" />
                             <div class="post-box-text">
                                 <span><a href="#">Culture</a></span>
                                 <h3><a href="#">Amazon and Snapchat rank low for protecting user data from government</a></h3>
@@ -35,7 +35,7 @@ class layout_homepage_hero extends layout
                             </div>
                         </li>
                         <li>
-                            <img src="demo/785x505.gif" alt="Image" />
+                            <img src="/785/505/filler.jpg" alt="Image" />
                             <div class="post-box-text">
                                 <span><a href="#">Culture</a></span>
                                 <h3><a href="#">Amazon and Snapchat rank low for protecting user data from government</a></h3>
@@ -43,7 +43,7 @@ class layout_homepage_hero extends layout
                             </div>
                         </li>
                         <li>
-                            <img src="demo/785x505.gif" alt="Image" />
+                            <img src="/785/505/filler.jpg" alt="Image" />
                             <div class="post-box-text">
                                 <span><a href="#">Culture</a></span>
                                 <h3><a href="#">Amazon and Snapchat rank low for protecting user data from government</a></h3>
@@ -56,7 +56,7 @@ class layout_homepage_hero extends layout
                 <!-- Block with 2 posts -->
                 <div class="block-with-two-posts">
                     <div class="post-block">
-                        <a href="#"><img src="demo/390x250.gif" alt="Post"/></a>
+                        <a href="#"><img src="/390/250/filler.jpg" alt="Post"/></a>
                         <div class="post-box-text">
                             <span><a href="#">Culture</a></span>
                             <h3><a href="#">Britain's Got Talent: will Piers Morgan return to replace David Walliams?</a></h3>
@@ -64,7 +64,7 @@ class layout_homepage_hero extends layout
                         </div>
                     </div>
                     <div class="post-block">
-                        <a href="#"><img src="demo/390x250.gif" alt="Post"/></a>
+                        <a href="#"><img src="/390/250/filler.jpg" alt="Post"/></a>
                         <div class="post-box-text">
                             <span><a href="#">Sport</a></span>
                             <h3><a href="#">Wenger to sign new Arsenal three-year deal worth £24m</a></h3>
