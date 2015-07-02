907ebf43f768da713d5baff6ba38a0d2ef3c17b6
Andrea Passante <andrea.passante@firmstep.com>
2015-07-02 16:41:44 +0100

    Basic models finished

--- model/model_user.php
+++ model/model_user.php
@@ -9,22 +9,60 @@
 class model_user extends model
 {
 
-    static public function create()
+    static public function create( data_user $data)
     {
 
-        
+        $sql = '
+            INSERT INTO `user`
+                (  username,  password,  first_name,  last_name,  email )
+            VALUES
+                ( :username, :password, :first_name, :last_name, :email )
+        ';
+
+        $query = db::prepare( $sql );
+        $query
+            ->bindString( ':username', $data->username )
+            ->bindString( ':password', security::hash( $data->password ) )
+            ->bindString( ':first_name', $data->first_name )
+            ->bindString( ':last_name', $data->last_name )
+            ->bindString( ':email', $data->email )
+            ->execute();
+
+        return db::lastInsertId();
 
     }
 
-    static public function update()
+    static public function update( data_user $data, $newPassword=false )
     {
+
+        $sql = '
+            UPDATE `user`
+            SET username = :username,
+                password = :password,
+                first_name = :first_name,
+                last_name = :last_name,
+                email = :email
+            WHERE id = :id
+        ';
+
+        $password = $newPassword ? security::hash( $data->password ) : $data->password;
+
+        $query = db::prepare( $sql );
+        $query
+            ->bindString( ':username',   $data->username )
+            ->bindString( ':password',   $password )
+            ->bindString( ':first_name', $data->first_name )
+            ->bindString( ':last_name',  $data->last_name )
+            ->bindString( ':email',      $data->email )
+            ->bindInt   ( ':id',         $data->id )
+            ->execute();
 
     }
 
     static public function delete( $id )
     {
 
-        $sql = 'DELETE FROM user WHERE id = :id';
+        $sql = 'DELETE FROM `user` WHERE id = :id';
 
         $query = db::prepare( $sql );
         $query->bindInt( ':id', $id )->execute();
@@ -34,7 +72,7 @@ class model_user extends model
     static public function getById( $id )
     {
 
-        $sql = 'SELECT * FROM user WHERE id = :id';
+        $sql = 'SELECT * FROM `user` WHERE id = :id';
 
         $query = db::prepare( $sql );
         $query->bindInt( ':id', $id )->execute();
