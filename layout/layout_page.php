8d00a20ba79843a7137c44dd037cf1536aa04e23
Andrea Passante <andrea.passante@firmstep.com>
2015-07-06 13:00:51 +0100

    Added google stuff. Still untested

--- layout/layout_page.php
+++ layout/layout_page.php
@@ -62,6 +62,8 @@ abstract class layout_page extends layout
             '<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.0/jquery.min.js"></script>',
             '<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>',
 
+            '<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>',
+
         '</head>',
         '<body>',
 
--- tools/class_finder.php
+++ tools/class_finder.php
@@ -10,7 +10,7 @@ class class_finder
 
         switch( $frags[0] )
         {
-            case 'adwords':  require_once __DIR__ . '/adwords.php';  break;
+            case 'google':   require_once __DIR__ . '/google.php';   break;
             case 'constant': require_once __DIR__ . '/constant.php'; break;
             case 'message':  require_once __DIR__ . '/message.php';  break;
             case 'router':   require_once __DIR__ . '/router.php';   break;
