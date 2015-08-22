7176bd86435a99352aac13651319baea1d5b25d4
Andrea Passante <zante@inwind.it>
2015-08-22 14:59:31 +0100

    google analytics

--- layout/layout_page.php
+++ layout/layout_page.php
@@ -89,8 +89,41 @@ abstract class layout_page extends layout
             '<script src="/js/isotope.js"></script>',
             '<script src="/js/fitvids.min.js"></script>',
             '<script src="/js/contact.form.js"></script>',
-            '<script src="/js/init.js"></script>',
+            '<script src="/js/init.js"></script>';
 
+            switch( constant::$text['site'] )
+            {
+                case 'athena':
+                    echo '<script>',
+
+                        "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){",
+                        '(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),',
+                        'm=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)',
+                        "})(window,document,'script','//www.google-analytics.com/analytics.js','ga');",
+
+                        "ga('create', 'UA-15643626-12', 'auto');",
+                        "ga('send', 'pageview');",
+
+                    '</script>';
+                    break;
+
+                case 'hermes':
+                    echo '<script>',
+
+                        "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){",
+                        '(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),',
+                        'm=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)',
+                        "})(window,document,'script','//www.google-analytics.com/analytics.js','ga');",
+
+                        "ga('create', 'UA-15643626-13', 'auto');",
+                        "ga('send', 'pageview');",
+
+                    '</script>';
+                    break;
+
+            }
+
+        echo
         '</body>',
         '</html>';
 
