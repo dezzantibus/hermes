506ab62335cc0bf72a643ca8fb57ad3eb06103f2
Andrea Passante <andrea.passante@firmstep.com>
2015-09-10 14:17:37 +0100

    Fixed banners for athena

--- tools/banner.php
+++ tools/banner.php
@@ -56,7 +56,17 @@ class banner
             case 'athena':
                 switch( $data )
                 {
+
+                    case 'side2':
+                        echo
+                        '<SCRIPT charset="utf-8" type="text/javascript" src="http://ws-eu.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&MarketPlace=GB&ID=V20070822%2FGB%2Fathnew-21%2F8009%2F3fa34915-fa49-453d-a069-4780ac20a6a4&Operation=GetScriptTemplate"> </SCRIPT>',
+                        '<NOSCRIPT><A HREF="http://ws-eu.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&MarketPlace=GB&ID=V20070822%2FGB%2Fathnew-21%2F8009%2F3fa34915-fa49-453d-a069-4780ac20a6a4&Operation=NoScript">Amazon.co.uk Widgets</A></NOSCRIPT>';
+//                        '<script type="text/javascript"><!-- amazon_ad_tag = "athnew-21"; amazon_ad_width = "160"; amazon_ad_height = "600"; amazon_ad_link_target = "new";//--></script>',
+//                        '<script type="text/javascript" src="http://ir-uk.amazon-adsystem.com/s/ads.js"></script>';
+                        break;
+
                     case 'side1':
+                    default:
                         echo
                         '<ins class="adsbygoogle" ',
                             'style="display:block" ',
@@ -68,13 +78,6 @@ class banner
                         '</script>';
                         break;
 
-                    case 'side2':
-                        echo
-                        '<SCRIPT charset="utf-8" type="text/javascript" src="http://ws-eu.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&MarketPlace=GB&ID=V20070822%2FGB%2Fathnew-21%2F8009%2F3fa34915-fa49-453d-a069-4780ac20a6a4&Operation=GetScriptTemplate"> </SCRIPT>',
-                        '<NOSCRIPT><A HREF="http://ws-eu.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&MarketPlace=GB&ID=V20070822%2FGB%2Fathnew-21%2F8009%2F3fa34915-fa49-453d-a069-4780ac20a6a4&Operation=NoScript">Amazon.co.uk Widgets</A></NOSCRIPT>';
-//                        '<script type="text/javascript"><!-- amazon_ad_tag = "athnew-21"; amazon_ad_width = "160"; amazon_ad_height = "600"; amazon_ad_link_target = "new";//--></script>',
-//                        '<script type="text/javascript" src="http://ir-uk.amazon-adsystem.com/s/ads.js"></script>';
-                        break;
 
                 }
                 break;
